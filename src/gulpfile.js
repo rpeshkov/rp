919afbecadad9209743ace6bc1712437abab9dc2
Roman Peshkov <peshkovroman@gmail.com>
2018-04-10 01:03:34 +0200

    Initial commit

---
+++ src/gulpfile.js
@@ -0,0 +1,24 @@
+var autoprefix = require('gulp-autoprefixer'),
+    gulp = require('gulp'),
+    bourbon = require('bourbon').includePaths,
+    neat = require('bourbon-neat').includePaths,
+    sass = require('gulp-sass');
+
+var paths = {
+    scss: './stylesheets/**/*.scss'
+};
+
+gulp.task('sass', function () {
+    return gulp.src(['./stylesheets/main.scss'])
+        .pipe(sass({
+            sourcemaps: true,
+            includePaths: [bourbon, neat],
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(gulp.dest('../static/css'));
+});
+
+gulp.task('default', ['sass'], function () {
+    gulp.watch([paths.scss], ['sass']);
+});
